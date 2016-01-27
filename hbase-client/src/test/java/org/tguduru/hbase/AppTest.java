f90f1582480cba8d1f70353404d2a449d8e408eb
Thirupathi Reddy Guduru <thirupathi.guduru@cerner.com>
2016-01-26 22:03:07 -0600

    Added module for client API usage

---
+++ hbase-client/src/test/java/org/tguduru/hbase/AppTest.java
@@ -0,0 +1,13 @@
+package org.tguduru.hbase;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void testSomething(){
+        System.out.println("Passed");
+    }
+}
